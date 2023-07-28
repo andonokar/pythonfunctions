e6ce983fb24eaad4ef17d1f54084b3abd156b59e
andonokar <anderson.lima@domrock.ai>
2023-07-28 18:22:07 -0300

    basic configs for writing avro files

---
+++ variables.py
@@ -0,0 +1,6 @@
+escrita = {
+    "bucket_avro": "bucket to write avro files",
+    "bucket_errors": "bucket to write mismatched schema rows",
+    "write_format_avro": "wb",
+    "write_format_csv": "w"
+}
