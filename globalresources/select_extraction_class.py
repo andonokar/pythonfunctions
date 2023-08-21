b13503539267e6dd1056779f7db22a9ee7cb4b9a
andonokar <anderson.lima@domrock.ai>
2023-08-21 16:17:55 -0300

    documentation in the code

--- globalresources/select_extraction_class.py
+++ globalresources/select_extraction_class.py
@@ -14,8 +14,10 @@ class SelectClassExtraction:
         """
         fmsg = f'{SelectClassExtraction.__name__}.{self.get_class.__name__}'
         logger = log.createLogger(fmsg)
+        # Getting the extractor class
         extraction_class = self.conf.get('class', 'CsvExcelExtractor')
         check_class = globals().get(extraction_class)
+        # Checking if the class exists
         if not check_class or not isinstance(check_class, type):
             logger.error(f'A classe {extraction_class} nao existe')
             raise NotImplementedError(f'A classe {extraction_class} nao existe')
