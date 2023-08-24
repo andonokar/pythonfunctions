bd71efe0a6fe0e81b75723dbe810cc98e97d6555
andonokar <anderson.lima@domrock.ai>
2023-08-24 12:38:04 -0300

    now writes to kafka only if you set a topic in escrita configuration, and completely ignores if you dont set a topic for the client

--- globalresources/readredirect.py
+++ globalresources/readredirect.py
@@ -3,7 +3,7 @@ from avro_writer import Escrita
 from globalresources.reader_client import Client
 from globalresources.select_extraction_class import SelectClassExtraction
 from cloud.basic_s3_functions import move_file_s3
-# from util.log_kafka import createloggerforkafka
+from util.log_kafka import createloggerforkafka
 
 
 def read_and_redirect(bucket, file, key):
@@ -24,23 +24,25 @@ def read_and_redirect(bucket, file, key):
         writer = Escrita(tables, escrita_conf)
         writer.escreve()
     except Exception as err:
-        log_args = {
-            'nome_arquivo': key,
-            'S3_ini': bucket,
-            'S3_fim': escrita_conf["bucket_errors"],
-            'etapa': 'landing-zone'
-        }
-        # logger = createloggerforkafka(read_and_redirect.__name__, topic=escrita_conf["topic"], **log_args)
-        # logger.error(str(err))
+        if escrita_conf.get('topic'):
+            log_args = {
+                'nome_arquivo': key,
+                'S3_ini': bucket,
+                'S3_fim': escrita_conf["bucket_errors"],
+                'etapa': 'landing-zone'
+            }
+            logger = createloggerforkafka(read_and_redirect.__name__, topic=escrita_conf["topic"], **log_args)
+            logger.error(str(err))
         move_file_s3(bucket, escrita_conf["bucket_errors"], key, f'{escrita_conf["prefixname"]}{key}')
         raise Exception(str(err))
     else:
-        log_args = {
-            'nome_arquivo': key,
-            'S3_ini': bucket,
-            'S3_fim': escrita_conf["destinationbucket"],
-            'etapa': 'landing-zone'
-        }
-        # logger = createloggerforkafka(read_and_redirect.__name__, topic=escrita_conf["topic"], **log_args)
-        # logger.info('processado ok')
+        if escrita_conf.get('topic'):
+            log_args = {
+                'nome_arquivo': key,
+                'S3_ini': bucket,
+                'S3_fim': escrita_conf["destinationbucket"],
+                'etapa': 'landing-zone'
+            }
+            logger = createloggerforkafka(read_and_redirect.__name__, topic=escrita_conf["topic"], **log_args)
+            logger.info('processado ok')
         move_file_s3(bucket, escrita_conf["destinationbucket"], key, f'{escrita_conf["prefixname"]}{key}')
