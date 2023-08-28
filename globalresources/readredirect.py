3f78185cc5c30e67465d1288d81c3b576b0dc78d
andonokar <anderson.lima@domrock.ai>
2023-08-28 11:44:35 -0300

    set warnings to check possible mistakes better in lambda

--- globalresources/readredirect.py
+++ globalresources/readredirect.py
@@ -4,6 +4,7 @@ from globalresources.reader_client import Client
 from globalresources.select_extraction_class import SelectClassExtraction
 from cloud.basic_s3_functions import move_file_s3
 from util.log_kafka import createloggerforkafka
+from util import log
 
 
 def read_and_redirect(bucket, file, key):
@@ -16,13 +17,20 @@ def read_and_redirect(bucket, file, key):
     :param key: o caminho do arquivo no s3
     :return:
     """
+    fmsg = f'{read_and_redirect.__name__}'
+    logger = log.createLogger(fmsg)
     client = Client(bucket, key)
+    logger.warning('client ok')
     escrita_conf, file_conf = client.get_conf()
+    logger.warning('conf ok')
     extrator = SelectClassExtraction(file_conf).get_class()
+    logger.warning('class ok')
     try:
         tables = CriaDataFrame(extrator, file, key, file_conf).extrair_para_avro()
+        logger.warning('extraction ok')
         writer = Escrita(tables, escrita_conf)
         writer.escreve()
+        logger.warning('avro ok')
     except Exception as err:
         if escrita_conf.get('topic'):
             log_args = {
@@ -31,8 +39,11 @@ def read_and_redirect(bucket, file, key):
                 'S3_fim': escrita_conf["bucket_errors"],
                 'etapa': 'landing-zone'
             }
+            logger.warning('trying kafka')
             createloggerforkafka(str(err), 'error', topic=escrita_conf["topic"], **log_args)
+            logger.warning('sucess kafka')
         move_file_s3(bucket, escrita_conf["bucket_errors"], key, f'{escrita_conf["prefixname"]}{key}')
+        logger.warning('file moved')
         raise Exception(str(err))
     else:
         if escrita_conf.get('topic'):
@@ -42,5 +53,8 @@ def read_and_redirect(bucket, file, key):
                 'S3_fim': escrita_conf["destinationbucket"],
                 'etapa': 'landing-zone'
             }
+            logger.warning('trying kafka')
             createloggerforkafka('processado ok', 'info', topic=escrita_conf["topic"], **log_args)
+            logger.warning('sucess kafka')
         move_file_s3(bucket, escrita_conf["destinationbucket"], key, f'{escrita_conf["prefixname"]}{key}')
+        logger.warning('file moved')
