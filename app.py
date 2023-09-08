18c472147f6a680e75820a7b947feada71fe4a5f
andonokar <anderson.lima@domrock.ai>
2023-09-08 16:08:55 -0300

    remove some useless code

--- app.py
+++ app.py
@@ -6,8 +6,6 @@ from cloud.basic_s3_functions import read_file_from_s3_object
 def handler(event, context):
     bucket = event['Records'][0]['s3']['bucket']['name']
     key = event['Records'][0]['s3']['object']['key']
-    table_name = key.split('/')
-    print(table_name[0])
     print(f'{bucket}/{key}')
     file = read_file_from_s3_object(bucket, key)
     buffer = io.BytesIO(file)
