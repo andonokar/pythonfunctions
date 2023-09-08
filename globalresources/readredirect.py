7920662242937bbf5acb719f03be79d07f34a310
andonokar <anderson.lima@domrock.ai>
2023-09-08 16:20:44 -0300

    now implements new dataframe class

--- globalresources/readredirect.py
+++ globalresources/readredirect.py
@@ -6,6 +6,7 @@ from cloud.basic_s3_functions import move_file_s3
 from util.log_kafka import createloggerforkafka
 from util import log
 from io import BytesIO
+from globalresources.process_dataframe import read_dataframe
 
 
 def read_and_redirect(bucket: str, file: str | BytesIO, key: str) -> None:
@@ -27,7 +28,8 @@ def read_and_redirect(bucket: str, file: str | BytesIO, key: str) -> None:
     extrator = SelectClassExtraction(file_conf).get_class()
     logger.warning('class ok')
     try:
-        tables = CriaDataFrame(extrator, file, key, file_conf).extrair_para_avro()
+        processdf = read_dataframe(file, key, file_conf)
+        tables = CriaDataFrame(extrator, processdf).extrair_para_avro()
         logger.warning('extraction ok')
         writer = Escrita(tables, escrita_conf)
         mistakes = writer.escreve()
@@ -45,7 +47,7 @@ def read_and_redirect(bucket: str, file: str | BytesIO, key: str) -> None:
             logger.warning('sucess kafka')
         move_file_s3(bucket, escrita_conf["bucket_errors"], key, f'{escrita_conf["prefixname"]}{key}')
         logger.warning('file moved with error')
-        logger.error(str(err))
+        logger.critical(str(err))
         raise Exception(str(err))
     else:
         if escrita_conf.get('topic'):
