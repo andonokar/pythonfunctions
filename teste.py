a8e22c9561637bc5da1e71ee11d2d70e5dfa067f
andonokar <anderson.lima@domrock.ai>
2023-09-08 16:08:35 -0300

    test lambda function locally

--- teste.py
+++ teste.py
@@ -1,21 +1,14 @@
-# import yaml
-# from avro.schema import parse
-# import json
-# with open("C:/Users/Anderson/Downloads/telecom.yaml", 'r') as r:
-#     config = yaml.safe_load(r)
-#
-# teste = config['rbx_ContratosMotivos']
-# print(teste)
-# csv_options = teste.get('csv')
-# print(csv_options)
-# header = csv_options.get('header')
-# print(header)
-
-def func(**kwargs):
-    data = kwargs
-    a = {'a': 1}
-    data = {**data, **a}
-    print(data)
-
-
-func(ronaldo=3)
+from cloud.basic_s3_functions import read_file_from_s3_object
+from io import BytesIO
+from globalresources.readredirect import read_and_redirect
+import boto3
+s3 = boto3.client('s3')
+bucket = "pjus-datalake"
+response = s3.list_objects_v2(Bucket=bucket, Prefix=f"")
+folder_array = [obj['Key'] for obj in response['Contents']]
+file_array = [i for i in folder_array if i[-1] != '/']
+for key in file_array:
+    file = read_file_from_s3_object(bucket, key)
+    buffer = BytesIO(file)
+    read_and_redirect(bucket, buffer, key)
+    break
