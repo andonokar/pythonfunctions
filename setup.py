ba2ef1e16c650eb7504991fcf48e103108a39f47
andonokar <anderson.lima@domrock.ai>
2023-09-18 11:43:31 -0300

    removed test.py from building

--- setup.py
+++ setup.py
@@ -4,5 +4,5 @@ from Cython.Build import cythonize
 setup(
     name='test-app',
     ext_modules=cythonize([f"*.py", '*/*.py'],
-                          ['__init__.py', '*/__init__.py', 'setup.py', '*/setup.py', 'app.py', 'main.py'])
+                          ['__init__.py', '*/__init__.py', 'setup.py', '*/setup.py', 'app.py', 'main.py', 'test.py'])
 )
