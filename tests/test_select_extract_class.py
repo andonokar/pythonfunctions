c177b4a40089b2c4dd741a74be0d6837b299bb70
andonokar <anderson.lima@domrock.ai>
2023-09-25 20:05:37 -0300

    test for selecting extraction class

---
+++ tests/test_select_extract_class.py
@@ -0,0 +1,30 @@
+import pytest
+from globalresources.select_extraction_class import SelectClassExtraction
+import yaml
+
+
+def test_select_extract_class():
+    with open("tests/dummy_files/test_client_class.yaml") as file:
+        response = yaml.safe_load(file)
+    test_class = SelectClassExtraction()
+    result = test_class.get_class(response['dummy_files'])
+    assert result.__name__ == 'CsvExcelExtractor'
+
+
+def test_select_extract_class_wrong_extractor():
+    with pytest.raises(NotImplementedError):
+        with open("tests/dummy_files/test_client_class.yaml") as file:
+            response = yaml.safe_load(file)
+        test_class = SelectClassExtraction()
+        response['dummy_files']['class'] = 'pao'
+        result = test_class.get_class(response['dummy_files'])
+        assert result.__name__ == 'CsvExcelExtractor'
+
+
+def test_select_extract_class_empty():
+    with open("tests/dummy_files/test_client_class.yaml") as file:
+        response = yaml.safe_load(file)
+    test_class = SelectClassExtraction()
+    response['dummy_files'].pop('class')
+    result = test_class.get_class(response['dummy_files'])
+    assert result.__name__ == 'CsvExcelExtractor'
