a6203b1209d18c1de1ac9ca9d5d12ab6afed7598
andonokar <anderson.lima@domrock.ai>
2023-09-25 10:25:01 -0300

    now uses yaml reader class to get the correct provider for reading the first yaml config

--- variables.py
+++ variables.py
@@ -1,3 +1,4 @@
-from cloud.basic_s3_functions import read_yaml_from_s3_object
-depara_config = read_yaml_from_s3_object("test-conf-domrock", "depara_conf.yaml")
-kafka_config = read_yaml_from_s3_object("test-conf-domrock", "kafka_config.yaml")
+from globalresources.yaml_reader import YamlReader
+reader = YamlReader('aws')
+depara_config = reader.read_yaml_file("test-conf-domrock", "depara_conf.yaml")
+kafka_config = reader.read_yaml_file("test-conf-domrock", "kafka_config.yaml")
