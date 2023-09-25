c27d28eef521700ac432be29942fa40c15390b38
andonokar <anderson.lima@domrock.ai>
2023-09-25 19:26:44 -0300

    custom exceptions

---
+++ exceptions/exceptions.py
@@ -0,0 +1,18 @@
+class YamlReadingError(Exception):
+    pass
+
+
+class FailedToMoveS3File(Exception):
+    pass
+
+
+class JsonReadingError(Exception):
+    pass
+
+
+class FileReadingError(Exception):
+    pass
+
+
+class FailedSavingFileToS3(Exception):
+    pass
