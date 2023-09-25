7abec78d00a29fb1f6c46d9e63dc136c794fdb2f
andonokar <anderson.lima@domrock.ai>
2023-09-25 10:26:07 -0300

    yaml reader class that reads yaml files based on the cloud provider

---
+++ globalresources/yaml_reader.py
@@ -0,0 +1,17 @@
+from cloud.basic_s3_functions import read_yaml_from_s3_object
+from cloud.basic_local_functions import read_yaml_local
+
+
+class YamlReader:
+    _options = {
+        'aws': read_yaml_from_s3_object,
+        'local': read_yaml_local
+    }
+
+    def __init__(self, provider: str):
+        if provider not in self._options.keys():
+            raise NotImplementedError(f'the {provider} provider is not accepted')
+        self._reader = self._options.get(provider)
+
+    def read_yaml_file(self, bucket: str, key: str):
+        return self._reader(bucket, key)
