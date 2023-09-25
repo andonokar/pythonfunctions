e7daf8924a1267a60b92ef70b928c1c0be959e9b
andonokar <anderson.lima@domrock.ai>
2023-09-25 19:26:59 -0300

    refactored

--- tests/test_client_class.py
+++ tests/test_client_class.py
@@ -1,21 +1,82 @@
 from globalresources.reader_client import Client
+from globalresources.yaml_reader import YamlReader
+import pytest
+import yaml
+
+from exceptions import exceptions
 
 
 def test_valitade_depara_config():
-    pass
+    client = Client('tests', 'dummy_files/addresses.csv')
+    result = client._validate_depara_config(
+        {'tests': {'bucket': 'tests', 'key': 'dummy_files/test_client_class.yaml'}})
+    assert result == {'bucket': 'tests', 'key': 'dummy_files/test_client_class.yaml'}
+
+
+def test_valitade_depara_config_error():
+    with pytest.raises(NotImplementedError):
+        client = Client('test', 'dummy_files/addresses.csv')
+        result = client._validate_depara_config(
+            {'tests': {'bucket': 'tests', 'key': 'dummy_files/test_client_class.yaml'}})
+        assert result == {'bucket': 'tests', 'key': 'dummy_files/test_client_class.yaml'}
+
+
+def test_read_client_conf_wrong_keys():
+    with pytest.raises(KeyError):
+        reader = YamlReader('local')
+        client = Client('tests', 'dummy_files/addresses.csv')
+        client._read_client_conf({'buckets': 'tests', 'key': 'dummy_files/test_client_class.yaml'}, reader)
+    with pytest.raises(KeyError):
+        reader = YamlReader('local')
+        client = Client('tests', 'dummy_files/addresses.csv')
+        client._read_client_conf({'bucket': 'tests', 'keys': 'dummy_files/test_client_class.yaml'}, reader)
+
+
+def test_read_client_conf_wrong_file():
+    with pytest.raises(exceptions.YamlReadingError):
+        reader = YamlReader('local')
+        client = Client('tests', 'dummy_files/addresses.csv')
+        client._read_client_conf({'bucket': 'tests', 'key': 'dummy_files/test_client_classwrong.yaml'}, reader)
 
 
 def test_read_client_conf():
-    pass
+    reader = YamlReader('local')
+    client = Client('tests', 'dummy_files/addresses.csv')
+    result = client._read_client_conf({'bucket': 'tests', 'key': 'dummy_files/test_client_class.yaml'}, reader)
+    with open('tests/dummy_files/test_client_class.yaml', 'r') as file:
+        check = yaml.safe_load(file)
+    assert result == check
 
 
 def test_validade_folder_conf():
-    pass
+    client = Client('tests', 'dummy_files/addresses.csv')
+    with open('tests/dummy_files/test_client_class.yaml', 'r') as file:
+        dicionario = yaml.safe_load(file)
+    result = client._validate_folder_conf(dicionario)
+    assert result == dicionario['dummy_files']
+
+
+def test_validade_folder_conf_wrong():
+    with pytest.raises(NotImplementedError):
+        client = Client('tests', 'dummy_file/addresses.csv')
+        with open('tests/dummy_files/test_client_class.yaml', 'r') as file:
+            dicionario = yaml.safe_load(file)
+        result = client._validate_folder_conf(dicionario)
+        assert result == dicionario['dummy_files']
 
 
 def test_def_validate_escrita_conf():
-    pass
+    client = Client('tests', 'dummy_files/addresses.csv')
+    with open('tests/dummy_files/test_client_class.yaml', 'r') as file:
+        dicionario = yaml.safe_load(file)
+    result = client._validate_escrita_conf(dicionario)
+    assert result == dicionario['escrita']
 
 
 def test_get_conf():
-    pass
+    client = Client('tests', 'dummy_files/addresses.csv')
+    reader = YamlReader('local')
+    result = client.get_conf({'tests': {'bucket': 'tests', 'key': 'dummy_files/test_client_class.yaml'}}, reader)
+    with open('tests/dummy_files/test_client_class.yaml', 'r') as file:
+        dicionario = yaml.safe_load(file)
+    assert result == (dicionario['escrita'], dicionario['dummy_files'])
--- tests/test_yaml_class.py
+++ tests/test_yaml_class.py
@@ -1,7 +1,23 @@
 from globalresources.yaml_reader import YamlReader
+import pytest
+from exceptions import exceptions
 
 
 def test_yaml_reader():
     reader = YamlReader('local')
     result = reader.read_yaml_file('tests', 'dummy_files/test_yaml_class.yaml')
     assert result == {'local': {'tests': {'bucket': 'tests', 'key': 'dummy_files/test_client_class.yaml'}}}
+
+
+def test_yaml_reader_wrong_option():
+    with pytest.raises(NotImplementedError):
+        reader = YamlReader('wrong')
+        result = reader.read_yaml_file('tests', 'dummy_files/test_yaml_class.yaml')
+        assert result == {'local': {'tests': {'bucket': 'tests', 'key': 'dummy_files/test_client_class.yaml'}}}
+
+
+def test_yaml_file_not_found():
+    with pytest.raises(exceptions.YamlReadingError):
+        reader = YamlReader('local')
+        result = reader.read_yaml_file('tests', 'dummy_files/test_yaml_clas.yaml')
+        assert result == {'local': {'tests': {'bucket': 'tests', 'key': 'dummy_files/test_client_class.yaml'}}}
