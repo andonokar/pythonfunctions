5fc12965bdc1897e68cecbd77130ebddcf3dbafa
andonokar <anderson.lima@domrock.ai>
2023-09-25 10:24:25 -0300

    test client class, still empty due to the design of new class yamlreader

---
+++ tests/test_client_class.py
@@ -0,0 +1,21 @@
+from globalresources.reader_client import Client
+
+
+def test_valitade_depara_config():
+    pass
+
+
+def test_read_client_conf():
+    pass
+
+
+def test_validade_folder_conf():
+    pass
+
+
+def test_def_validate_escrita_conf():
+    pass
+
+
+def test_get_conf():
+    pass
