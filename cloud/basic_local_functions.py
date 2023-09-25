f0d7cd313e6a6b97e8bf7c906dfd848427f5ff27
andonokar <anderson.lima@domrock.ai>
2023-09-25 10:21:25 -0300

    implemented a yaml reader for local environment

---
+++ cloud/basic_local_functions.py
@@ -0,0 +1,17 @@
+import yaml
+
+
+def read_yaml_local(bucket: str, key: str) -> dict:
+    """
+    Read YAML file in s3 bucket
+    :param bucket: s3 bucket name
+    :param key: filename
+    :return: YAML file as a python dictionary
+    """
+    try:
+        with open(f'{bucket}/{key}') as yaml_file:
+            yaml_object = yaml.safe_load(yaml_file)
+    except Exception as err:
+        raise Exception(f'Error while reading Bucket={bucket}, Key={key}: {err}')
+
+    return yaml_object
