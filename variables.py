fd3d67677e354b5da5990107f3f0b03bf4ae3020
andonokar <anderson.lima@domrock.ai>
2023-09-27 10:21:51 -0300

    now use provider

--- variables.py
+++ variables.py
@@ -1,4 +1,4 @@
-from globalresources.yaml_reader import YamlReader
-reader = YamlReader('aws')
-depara_config = reader.read_yaml_file("test-conf-domrock", "depara_conf.yaml")
-kafka_config = reader.read_yaml_file("test-conf-domrock", "kafka_config.yaml")
+from cloud.cria_provider import CriaProvider
+reader = CriaProvider().create_provider('aws')
+depara_config = reader.read_yaml_from_file("test-conf-domrock", "depara_conf.yaml")
+kafka_config = reader.read_yaml_from_file("test-conf-domrock", "kafka_config.yaml")
